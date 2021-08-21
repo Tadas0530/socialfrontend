f77bd5cd775b1ff68c8ecd7fe99f0eea9244887d
Tadas <tadasandrijauskass@gmail.com>
2021-08-21 15:42:54 +0300

    login/signup component implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,20 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { Routes, RouterModule } from '@angular/router';
 import { PostInputBoxComponent } from './components/post-input-box/post-input-box.component';
 import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './components/login/login.component';
+import { MainComponent } from './components/main/main.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 
 const routes: Routes = [
-   { path: 'category/:id', component: PostListComponent },
-   { path: 'category', component: PostListComponent },
-   { path: 'posts', component: PostListComponent },
+  {path: 'login', component: LoginComponent },
+  {path: 'signup', component: SignupComponent },
+  {path: '', component: MainComponent,
+  children: [
+    { path: 'category/:id', component: PostListComponent },
+    { path: 'category', component: PostListComponent },
+    { path: 'posts', component: PostListComponent },
+  ]},
 
 ];
 
@@ -23,7 +31,10 @@ const routes: Routes = [
     AppComponent,
     PostListComponent,
     PostCategoriesListComponent,
-    PostInputBoxComponent
+    PostInputBoxComponent,
+    LoginComponent,
+    MainComponent,
+    SignupComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
